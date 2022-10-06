97db2ac32b01ba82cb55291cc0d1ddb5c65092c4
Kellen Martin <kema0161@colorado.edu>
2022-10-06 16:03:22 -0600

    Outline for main workflow

--- Main.m
+++ Main.m
@@ -1,3 +1,18 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Main Script
+%
+% Authors: 
+%   Nick Aichholz
+%   Ryan Jones
+%   Kellen Martin
+%
+% Last Edited: 10/6/22
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Image Reconstruction 
+
+%% Image Detection 
+
+%% Image Compression
+
+%% Commands?
