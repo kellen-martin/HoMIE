abea4614c7d3f301fec6e65b70e676cab232ef23
nickaichholz <nickaichholz@engr2-2-51-dhcp.int.colorado.edu>
2022-10-14 11:43:38 -0600

    added comments

--- cleanFrames.m
+++ cleanFrames.m
@@ -1,26 +1,10 @@
 function [clean] = cleanFrames(images,avgImg)
+% [clean] = array of all clean frames
+% images = images takes by image sensor
+% avgImg = is the average of 'images'
 
-% path_directory=folder; % 'Folder name'
-% original_files=dir([path_directory '/*.png']); 
 sizeOf = length(images);
 
 for k=1:sizeOf
-%   filename=[path_directory '/' original_files(k).name];
-%   [image_orginalTemp{k},map{k}] = imread(filename);
-% %   gsImg{k} = rgb2gray(image_orginalTemp{k})
-%   image{k} = single(image_orginalTemp{k});
-  clean{k} = (avgImg - images{k});
+    clean{k} = (avgImg - images{k});
 end
-% function [clean] = cleanFrames(folder,avgImg)
-% 
-% path_directory=folder; % 'Folder name'
-% original_files=dir([path_directory '/*.png']); 
-% sizeOf = length(original_files);
-% 
-% for k=1:sizeOf
-%   filename=[path_directory '/' original_files(k).name];
-%   [image_orginalTemp{k},map{k}] = imread(filename);
-% %   gsImg{k} = rgb2gray(image_orginalTemp{k})
-%   image_orginal{k} = single(image_orginalTemp{k});
-%   clean{k} = (avgImg - image_orginal{k});
-% end
