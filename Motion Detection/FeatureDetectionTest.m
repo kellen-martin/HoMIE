2c0d4c452dc98e13984dfce317103f5186411737
Kellen Martin <kema0161@colorado.edu>
2022-10-20 16:22:08 -0600

    template for image detection functions

---
+++ Motion Detection/FeatureDetectionTest.m
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Image Detection
+% This script is a test of the vision toolbox 
+% Authors: 
+%   Kellen Martin
+% 
+% Last Edited: 10/20/22 
+%
+% The purpose of this script is to test the vision toolbox for the
+% idetification of objects in a single frame
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Load a sample hologram frame as a greyscale image (single frame from a single z-slice) 
+
