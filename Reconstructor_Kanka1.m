f2645c926a9c50df63166e58773191c01be1be8c
rjones2508 <ryjo8808@colorado.edu>
2022-11-02 17:27:42 -0600

    updated reconstructor to support tiles, added comments

--- Reconstructor_Kanka1.m
+++ Reconstructor_Kanka1.m
@@ -26,127 +26,124 @@ function [reconstructed] = Reconstructor_Kanka1(image,zslice,inputs,cleaner)
 % Last Edited: 10/25/22
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tic
 
 nTiles = 4; % Total tiles will be (nTiles * nTiles)
 
-tic
-
 inputs.ref_dist = inputs.ref_dist + inputs.z_resolution * (zslice-1);
-rval = CalculateTemplate(inputs);
 
 HoloTileSize = (inputs.n_pixels/inputs.OVS)/nTiles;
-RefTileSize = (size(rval.ref_wave, 1))/nTiles;
+RefTileSize = inputs.n_pixels;
 
-if nargin == 3   % if the number of inputs equals 2
-    image = single(image(1:inputs.Sn_pixels,1:inputs.Sn_pixels));
-    image = single(imresize(image,inputs.OVS));
-
-%     cleaner = single(cleaner(1:inputs.Sn_pixels,1:inputs.Sn_pixels));
-%     cleaner = single(imresize(cleaner,inputs.OVS));
-% 
-%     image = image - cleaner;
-
-    down_sz = inputs.n_pixels/inputs.OVS; %this is what sensor sees
-    I2_down = imresize(image - abs(rval.ref_wave).^2, [down_sz, down_sz], 'box'); %find intensity then remove its effect
-
-    I2_down_fft = fft2(I2_down); %of data
-    I2_up_fft   = single(zeros(inputs.n_pixels, inputs.n_pixels)); %insert zeros
-
-    %inverse fft to oversample
-    I2_up_fft(              1:down_sz/2, 1:down_sz/2)         = I2_down_fft(          1:down_sz/2, 1:down_sz/2);
-    I2_up_fft(end-down_sz/2+1:end      , 1:down_sz/2)         = I2_down_fft(down_sz/2+1:end      , 1:down_sz/2);
-    I2_up_fft(              1:down_sz/2, end-down_sz/2+1:end) = I2_down_fft(          1:down_sz/2, down_sz/2+1:end);
-    I2_up_fft(end-down_sz/2+1:end      , end-down_sz/2+1:end) = I2_down_fft(down_sz/2+1:end      , down_sz/2+1:end);
-    I2_up = ifft2(I2_up_fft);
-
-    despread = I2_up.*rval.ref_wave; % 'despreading' the signal
-    % if there was no interference, microscope would not work
-
-    % Begin clearing unused large variables to save memory:
-    clear I2_up;
-    clear I2_up_fft;
-
-    [X,Y] = meshgrid(single(linspace(-1,1,inputs.n_pixels)), single(linspace(-1,1,inputs.n_pixels)));
-    apodizer = single(sqrt(X.^2 + Y.^2) < 1);
-
-    clear X;
-    clear Y;
-
-    % Correlate against template, use apodizer
-    despread_fft = fft2(despread.*apodizer);
-    template_fft = fft2(rval.template_wave.*apodizer);
-    clear apodizer;
-    clear despread;
-
-    % fftshift rearranged image into a state we can understand
-    correlated = fftshift(ifft2(despread_fft.*conj(template_fft))); % frequency-domain convolution (correlation)
-    reconstructed = correlated;
-    clear despread_fft;
-    clear template_fft;             
+image = single(image(1:inputs.Sn_pixels,1:inputs.Sn_pixels));
+image = single(imresize(image,inputs.OVS));
+    
+if nargin == 3   % No cleaning argument
 end
-if nargin == 4   % if the number of inputs equals 2
-    image = single(image(1:inputs.Sn_pixels,1:inputs.Sn_pixels));
-    image = single(imresize(image,inputs.OVS));
-
+if nargin == 4   % cleaner argument used
     cleaner = single(cleaner(1:inputs.Sn_pixels,1:inputs.Sn_pixels));
     cleaner = single(imresize(cleaner,inputs.OVS));
 
     image = image - cleaner;
-
-    down_sz = inputs.n_pixels/inputs.OVS; %this is what sensor sees
-    I2_down = imresize(image - abs(rval.ref_wave).^2, [down_sz, down_sz], 'box'); %find intensity then remove its effect
-
-    %%%%%%%%%%%%%%%%%%%%%%% Split into Tiles here %%%%%%%%%%%%%%%%%%%%%%%%%
-    % current size is 3000 by 3000 here (down_sz)
-    
-    for i = 1:nTiles
-        % Tiles themselves arent saved in memory, finding edges here
-        
-        HoloTileStart(i) = floor((i - 1)*HoloTileSize) + 1;
-        HoloTileEnd(i) = ceil(i*HoloTileSize);
-        
-        RefTileStart(i) = floor((i - 1)*RefTileSize) + 1;
-        RefTileEnd(i) = ceil(i*RefTileSize);
-        
-    end
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    I2_down_fft = fft2(I2_down); %of data
-    I2_up_fft   = single(zeros(inputs.n_pixels, inputs.n_pixels)); %insert zeros
-
-    %inverse fft to oversample
-    I2_up_fft(              1:down_sz/2, 1:down_sz/2)         = I2_down_fft(          1:down_sz/2, 1:down_sz/2);
-    I2_up_fft(end-down_sz/2+1:end      , 1:down_sz/2)         = I2_down_fft(down_sz/2+1:end      , 1:down_sz/2);
-    I2_up_fft(              1:down_sz/2, end-down_sz/2+1:end) = I2_down_fft(          1:down_sz/2, down_sz/2+1:end);
-    I2_up_fft(end-down_sz/2+1:end      , end-down_sz/2+1:end) = I2_down_fft(down_sz/2+1:end      , down_sz/2+1:end);
-    I2_up = ifft2(I2_up_fft);
-
-    despread = I2_up.*rval.ref_wave; % 'despreading' the signal
-    % if there was no interference, microscope would not work
-
-    % Begin clearing unused large variables to save memory:
-    clear I2_up;
-    clear I2_up_fft;
-
-    [X,Y] = meshgrid(single(linspace(-1,1,inputs.n_pixels)), single(linspace(-1,1,inputs.n_pixels)));
-    apodizer = single(sqrt(X.^2 + Y.^2) < 1);
-
-    clear X;
-    clear Y;
-
-    % Correlate against template, use apodizer
-    despread_fft = fft2(despread.*apodizer);
-    template_fft = fft2(rval.template_wave.*apodizer);
-    clear apodizer;
-    clear despread;
-
-    % fftshift rearranged image into a state we can understand
-    correlated = fftshift(ifft2(despread_fft.*conj(template_fft))); % frequency-domain convolution (correlation)
-    reconstructed = correlated;
-    clear despread_fft;
-    clear template_fft;             
 end
+
+down_sz = inputs.n_pixels/inputs.OVS; %this is what sensor sees
+%     I2_down = imresize(image - abs(rval.ref_wave).^2, [down_sz, down_sz], 'box'); %find intensity then remove its effect
+% ^^^^ I believe this step is taken care of with the average frame
+       % stuff
+I2_down = image;
+
+%%%%%%%%%%%%%%%%%%%%%%%%% Split into Tiles here %%%%%%%%%%%%%%%%%%%%%%%%%%%
+% current size is 3000 by 3000 here (down_sz)
+
+for i = 1:nTiles
+    % Tiles themselves arent saved in memory, finding edges here
+
+    HoloTileStart(i) = floor((i - 1)*HoloTileSize) + 1;
+    HoloTileEnd(i) = ceil(i*HoloTileSize);
+
+    RefTileStart(i) = floor((i - 1)*RefTileSize) + 1;
+    RefTileEnd(i) = ceil(i*RefTileSize);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+TileSum = single(zeros(inputs.n_pixels));
+tileSize = floor(HoloTileSize);
+
+for i = 1:nTiles
+
+    for j = 1:nTiles
+
+        %%%%%%%%%%%%%%%%%%%% Each tile processed here %%%%%%%%%%%%%%%%%%%%%
+
+        % upsample each hologram "tile" to same size as reference "patch"
+            % make this 6000x6000   <???> 
+                % current output size
+                % reference wave is then (6000*nTiles)x(6000*nTiles)
+                    % LARGE!
+                    % maybe calculate the patch of reference as needed
+                        % save memory                        
+
+        % multiply this with ref wave and add to sum
+
+        % Selecting tile and allocating reference wave patch
+        tile_down_fft = fft2(I2_down(HoloTileStart(i):HoloTileEnd(i), HoloTileStart(j):HoloTileEnd(j)));
+        patch = single(zeros(inputs.n_pixels));
+
+        % CALCULATE REFERENCE PATCH HERE ----------------------------------
+        
+
+            
+                %           < CODE NOT DONE YET >
+
+                
+
+        % -----------------------------------------------------------------
+
+        tile_up_fft   = single(zeros(inputs.n_pixels));
+
+        tile_up_fft(              1:tileSize/2, 1:tileSize/2)         = tile_down_fft(          1:tileSize/2, 1:tileSize/2);
+        tile_up_fft(end-tileSize/2+1:end      , 1:tileSize/2)         = tile_down_fft(tileSize/2+1:end      , 1:tileSize/2);
+        tile_up_fft(              1:tileSize/2, end-tileSize/2+1:end) = tile_down_fft(          1:tileSize/2, tileSize/2+1:end);
+        tile_up_fft(end-tileSize/2+1:end      , end-tileSize/2+1:end) = tile_down_fft(tileSize/2+1:end      , tileSize/2+1:end);
+
+        tile_up = ifft2(tile_up_fft); % Upsampled tile
+
+        TileSum = TileSum + tile_up.*patch;
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    end
+
+end
+
+
+
+% Begin clearing unused large variables to save memory:
+clear tile_up;
+clear tile_up_fft;
+clear patch;
+clear tile_down_fft;
+
+[X,Y] = meshgrid(single(linspace(-1,1,inputs.n_pixels)), single(linspace(-1,1,inputs.n_pixels)));
+apodizer = single(sqrt(X.^2 + Y.^2) < 1);
+
+clear X;
+clear Y;
+
+% Correlate against template, use apodizer
+despread_fft = fft2(TileSum.*apodizer);
+template_fft = fft2(template_wave.*apodizer);
+clear apodizer;
+clear despread;
+
+% fftshift rearranged image into a state we can understand
+correlated = fftshift(ifft2(despread_fft.*conj(template_fft))); % frequency-domain convolution (correlation)
+reconstructed = correlated;
+clear despread_fft;
+clear template_fft;             
 
 time = toc;
 fprintf('\nReconstructor %.2f\n', time)
