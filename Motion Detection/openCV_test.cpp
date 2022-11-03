826ea3ea81b32f72073f686e090202058ae67f36
Kellen Martin <kema0161@colorado.edu>
2022-11-03 11:06:56 -0600

    creates a file to test openCV install

---
+++ Motion Detection/openCV_test.cpp
@@ -0,0 +1,12 @@
+#include <../opencv/build/include/opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv ;
+
+// This scirpt is to test the installation of openCV by testing imload and imshow
+void main {
+
+
+    return 0;
+}
