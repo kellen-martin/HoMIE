1387517248737cb72d561f5cd121d3a3ee669237
kellen-martin <kema0161@colorado.edu>
2022-11-07 13:12:30 -0700

    adds a test for openCV install

--- Motion Detection/openCV_test.cpp
+++ Motion Detection/openCV_test.cpp
@@ -1,12 +1,19 @@
-#include <../opencv/build/include/opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv-4.x/include/opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv ;
 
-// This scirpt is to test the installation of openCV by testing imload and imshow
-void main {
+// This scirpt is to test the installation of openCV by testing imread and imshow
+int main() {
+    // Defines image loaction
+    string path = "../generateData1/simulated_hologram1.png";
 
+    // Reads Image 
+    Mat img = imread(path);
+
+    // Displays Image
+    imshow("Image", img);
 
     return 0;
 }
