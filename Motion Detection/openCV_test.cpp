eb4d463413e3e6e4ba2dcd1fcf1ce81074f5bb1a
kellen-martin <kema0161@colorado.edu>
2022-11-09 13:32:56 -0700

    specifies to read greyscale

--- Motion Detection/openCV_test.cpp
+++ Motion Detection/openCV_test.cpp
@@ -10,7 +10,7 @@ int main() {
     string path = "../generatedData1/simulated_hologram1.png";
 
     // Reads Image 
-    Mat img = imread(path);
+    Mat img = imread(path, IMREAD_GRAYSCALE);
 
     // saves test image
     imwrite("/home/kells/HoMIE/Motion Detection/test.png", img);
