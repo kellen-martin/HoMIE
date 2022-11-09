f5dc8ac7e37e67a526bd708fb1c7bf4087439cc5
kellen-martin <kema0161@colorado.edu>
2022-11-09 13:03:03 -0700

    finishes opencv test and creates makefile

--- Motion Detection/openCV_test.cpp
+++ Motion Detection/openCV_test.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <opencv-4.x/include/opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
-using namespace cv ;
+using namespace cv;
 
-// This scirpt is to test the installation of openCV by testing imread and imshow
+// This scirpt is to test the installation of openCV by testing imread and imwrite
 int main() {
     // Defines image loaction
-    string path = "../generateData1/simulated_hologram1.png";
+    string path = "../generatedData1/simulated_hologram1.png";
 
     // Reads Image 
     Mat img = imread(path);
 
-    // Displays Image
-    imshow("Image", img);
-
+    // saves test image
+    imwrite("/home/kells/HoMIE/Motion Detection/test.png", img);
     return 0;
 }
