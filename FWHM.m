b183c492556bc9a92d5cc68b8967b923fa062b2b
ejlde <122563402+ejlde@users.noreply.github.com>
2023-02-22 16:53:35 -0700

    Name Change & FWHM Found
    Can find the FWHM manually.

--- FWHM.m
+++ FWHM.m
@@ -5,7 +5,7 @@ image = imread('reconimage.png');
 imshow(image);
 [cx,cy,c] = improfile(); % allows the user to draw a line in the image; 
 % outputs are the x,y pixel coords, and their respective light intensities
+halfmax = max(c)/2 ; 
 plot(cx,c) ; grid on; title('Light Intensity vs Pixel X Coordinate');xlabel('Pixel X Coordinate');ylabel('Pixel Y Coordinate');
-
-
+yline(halfmax,'r');
 %roi = drawline('Color','r');
