0947fecdf882d5c8bbe84d0aad03fc1c785292d1
ejlde <122563402+ejlde@users.noreply.github.com>
2023-02-22 16:28:56 -0700

    Update decon2.m

--- decon2.m
+++ decon2.m
@@ -1,7 +1,11 @@
 % Eric Dean
-% hello
 clc;clear all; close all;
 
 image = imread('reconimage.png');
-imshow(image); axis image;
-h_rect = image.roi.Rectangle ; 
+imshow(image);
+[cx,cy,c] = improfile(); % allows the user to draw a line in the image; 
+% outputs are the x,y pixel coords, and their respective light intensities
+plot(cx,c) ; grid on; title('Light Intensity vs Pixel X Coordinate');xlabel('Pixel X Coordinate');ylabel('Pixel Y Coordinate');
+
+
+%roi = drawline('Color','r');
