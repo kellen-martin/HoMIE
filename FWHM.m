5ca99102861259b6de02fb8264d6242cf5d18a65
ejlde <122563402+ejlde@users.noreply.github.com>
2023-02-23 08:48:46 -0700

    Autocalculates the fwhm

--- FWHM.m
+++ FWHM.m
@@ -5,7 +5,13 @@ image = imread('reconimage.png');
 imshow(image);
 [cx,cy,c] = improfile(); % allows the user to draw a line in the image; 
 % outputs are the x,y pixel coords, and their respective light intensities
-halfmax = max(c)/2 ; 
+
+[max_value, max_index] = max(c) ; 
+threshold = max_value/2 ; 
+lower_index = find(c(1:max_index) < threshold,1,'last');
+upper_index = find(c(max_index:end) < threshold,1) + max_index -1 ; 
+x_unit = cx(2) - cx(1) ; 
+fwhm = (upper_index - lower_index)*x_unit ; 
 plot(cx,c) ; grid on; title('Light Intensity vs Pixel X Coordinate');xlabel('Pixel X Coordinate');ylabel('Pixel Y Coordinate');
-yline(halfmax,'r');
+yline(threshold,'r');
 %roi = drawline('Color','r');
