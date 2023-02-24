b7505829243f5821efbed83d41eeaaec43d7536b
ejlde <122563402+ejlde@users.noreply.github.com>
2023-02-23 17:20:30 -0700

    Plots the intersection points

--- FWHM.m
+++ FWHM.m
@@ -1,7 +1,7 @@
 % Eric Dean
 clc;clear all; close all;
-
 image = imread('reconimage.png');
+figure(1)
 imshow(image);
 [cx,cy,c] = improfile(); % allows the user to draw a line in the image; 
 % outputs are the x,y pixel coords, and their respective light intensities
@@ -10,10 +10,14 @@ imshow(image);
 
 [max_value, max_index] = max(c) ; 
 threshold = max_value/2 ; 
-lower_index = find(c(1:max_index) < threshold,1,'last');
+lower_index = find(c(1:max_index) < threshold,1,'last'); %
 upper_index = find(c(max_index:end) < threshold,1) + max_index -1 ; 
 x_unit = cx(2) - cx(1) ; 
-fwhm = (upper_index - lower_index)*x_unit ; 
-plot(cx,c) ; grid on; title('Light Intensity vs Pixel X Coordinate');xlabel('Pixel X Coordinate');ylabel('Pixel Y Coordinate');
-yline(threshold,'r');
-%roi = drawline('Color','r');
+fwhm = (cx(upper_index) - cx(lower_index))*x_unit ; 
+figure(2); hold on;
+plot(cx,c) ; grid on; title('Sample Cross Section','FontSize',18);xlabel('Pixel X Coordinates','FontSize',18);ylabel('Light Intensity','FontSize',18);
+%plot(cx(max_index),threshold:max_value,'Marker','x','MarkerColor','b') ; 
+yline(threshold,'r','LineWidth',2);
+%xline(cx(max_index)) ; 
+plot(cx(lower_index+1),threshold,'Marker','x','MarkerEdgeColor','r');
+plot(cx(upper_index),threshold,'Marker','x','MarkerEdgeColor','r'); 
