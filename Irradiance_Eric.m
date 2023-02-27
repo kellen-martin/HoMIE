87c01e59ed9f62dbf339ec08942f41bf8ba488eb
ejlde <122563402+ejlde@users.noreply.github.com>
2023-02-27 12:19:52 -0700

    Update

--- Irradiance_Eric.m
+++ Irradiance_Eric.m
@@ -1,1 +1,36 @@
 % Irradiance Function
+% The purpose of this script is to create a beam expansion and irradiance
+% model that incorporates distance along the optical axis, and lens
+% properties
+% 2/27/2023
+
+% Laser Parameters
+NA_laser = 0.13/1000; % [mrad -> rad]
+Fiber_diam = 9e-6 ; %[um -> m]
+Out_P = 3/1000 ; % [mW->W]
+
+% Collimator Parameters
+r_c = 0.98/1000 ; %[mm - > m]
+
+% Focus Lens Parameters
+FL_focus = 2/1000 ; %[mm - > m]
+
+% Radius from the laser to the collimator
+z = 0:1:7.5 ; % [mm]
+r_z = z*tan(NA_laser) + Fiber_diam ; 
+
+% Irradiance from the laser to the collimator
+Irr_col = Out_P/(pi*r_z^2) ; 
+
+% Refraction angle from the focus lens
+theta = (r_c/FL_focus) ; % 
+
+% Radius from the focus lens to the image sensor
+z_lensless = 0:2:21 ; % [mm] 
+r_z_lensless = z_lensless*tan(theta) ; 
+
+% Irradiance from the focus lens to the sample housing
+Irr_sens = (Out_P*T_C*T_FL)/(pi*r_z_lensless^2) ; % 
+
+
+
