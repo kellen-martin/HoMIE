8b2f7de12f89f954d75e9c628a9231911a67ebe5
ejlde <122563402+ejlde@users.noreply.github.com>
2023-03-10 13:20:22 -0700

    Update

--- Images to Power Output/Image_Processing.m
+++ Images to Power Output/Image_Processing.m
@@ -5,4 +5,4 @@ clc;clear all; close all;
 image = imread('Direct View 3_9 0d05ms.tif');
 whos image
 info = imfinfo('Direct View 3_9 0d05ms.tif');
-imhist(image,8) % intensity distribution in the image
+imhist(image) % intensity distribution in the image
