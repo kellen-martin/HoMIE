c9fdb55192ef571813285c1963a2c5753ea2227b
kellen-martin <kema0161@colorado.edu>
2023-03-22 11:43:34 -0600

    removes matlab files

--- Matlab/Writer.m
+++
@@ -1,7 +0,0 @@
-function Writer(image,name)
-    I = sqrt(abs(image));
-    J = I / max(I(:));
-    J = uint8(round(J * 255));
-    J = 255 - J;
-    imwrite(J, colormap(gray(256)), [name '.png'], 'BitDepth', 8);
-end
